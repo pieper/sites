465d809d92fdb7a4968a55a7bffe7b8e8aa36ae5
Steve Pieper <pieper@isomics.com>
2016-08-22 20:13:12 -0400

    Add matrix math and basic view controls

--- step/view.js
+++ step/view.js
@@ -63,6 +63,64 @@ class View {
     return([v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]]);
   }
 
+  midentity() {
+    return ([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]);
+  }
+
+  mtranslate(v) {
+    return ([[1,0,0,0],[0,1,0,0],[0,0,1,0],[v[0],v[1],v[2],1]]);
+  }
+
+  mscale(v) {
+    return ([[v[0],0,0,0],[0,v[1],0,0],[0,0,v[2],0],[0,0,0,1]]);
+  }
+
+  // return a matrix to rotate a point around the axis by angle
+  // axis must be normalized
+  // angle is in degrees
+  // https://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
+  mrotate(u, theta) {
+    let T = theta * Math.PI/180.;
+    let cT = Math.cos(T);
+    let sT = Math.sin(T);
+    return ([
+      [cT+u[0]*u[0]*(1-cT), u[1]*u[0]*(1-cT)+u[2]*sT, u[2]*u[0]*(1-cT)-u[1]*sT, 0],
+      [u[0]*u[1]*(1-cT)-u[2]*sT, cT+u[1]*u[1]*(1-cT), u[2]*u[1]*(1-cT)+u[0]*sT, 0],
+      [u[0]*u[2]*(1-cT)+u[1]*sT, u[1]*u[2]*(1-cT)-u[0]*sT, cT+u[2]*u[2]*(1-cT), 0],
+      [0, 0, 0, 1]
+    ]);
+  }
+
+  mmultiply(m1, m2) {
+    return ([
+      [m1[0][0]*m2[0][0]+m1[1][0]*m2[0][1]+m1[2][0]*m2[0][2]+m1[3][0]*m2[0][3],
+       m1[0][1]*m2[0][0]+m1[1][1]*m2[0][1]+m1[2][1]*m2[0][2]+m1[3][1]*m2[0][3],
+       m1[0][2]*m2[0][0]+m1[1][2]*m2[0][1]+m1[2][2]*m2[0][2]+m1[3][2]*m2[0][3],
+       m1[0][3]*m2[0][0]+m1[1][3]*m2[0][1]+m1[2][3]*m2[0][2]+m1[3][3]*m2[0][3]],
+      [m1[0][0]*m2[1][0]+m1[1][0]*m2[1][1]+m1[2][0]*m2[1][2]+m1[3][0]*m2[1][3],
+       m1[0][1]*m2[1][0]+m1[1][1]*m2[1][1]+m1[2][1]*m2[1][2]+m1[3][1]*m2[1][3],
+       m1[0][2]*m2[1][0]+m1[1][2]*m2[1][1]+m1[2][2]*m2[1][2]+m1[3][2]*m2[1][3],
+       m1[0][3]*m2[1][0]+m1[1][3]*m2[1][1]+m1[2][3]*m2[1][2]+m1[3][3]*m2[1][3]],
+      [m1[0][0]*m2[2][0]+m1[1][0]*m2[2][1]+m1[2][0]*m2[2][2]+m1[3][0]*m2[2][3],
+       m1[0][1]*m2[2][0]+m1[1][1]*m2[2][1]+m1[2][1]*m2[2][2]+m1[3][1]*m2[2][3],
+       m1[0][2]*m2[2][0]+m1[1][2]*m2[2][1]+m1[2][2]*m2[2][2]+m1[3][2]*m2[2][3],
+       m1[0][3]*m2[2][0]+m1[1][3]*m2[2][1]+m1[2][3]*m2[2][2]+m1[3][3]*m2[2][3]],
+      [m1[0][0]*m2[3][0]+m1[1][0]*m2[3][1]+m1[2][0]*m2[3][2]+m1[3][0]*m2[3][3],
+       m1[0][1]*m2[3][0]+m1[1][1]*m2[3][1]+m1[2][1]*m2[3][2]+m1[3][1]*m2[3][3],
+       m1[0][2]*m2[3][0]+m1[1][2]*m2[3][1]+m1[2][2]*m2[3][2]+m1[3][2]*m2[3][3],
+       m1[0][3]*m2[3][0]+m1[1][3]*m2[3][1]+m1[2][3]*m2[3][2]+m1[3][3]*m2[3][3]]
+    ]);
+  }
+
+  mvmultiply(m, v) {
+    return ([
+      m[0][0]*v[0]+m[1][0]*v[1]+m[2][0]*v[2]+m[3][0]*v[3],
+      m[0][1]*v[0]+m[1][1]*v[1]+m[2][1]*v[2]+m[3][1]*v[3],
+      m[0][2]*v[0]+m[1][2]*v[1]+m[2][2]*v[2]+m[3][2]*v[3],
+      m[0][3]*v[0]+m[1][3]*v[1]+m[2][3]*v[2]+m[3][3]*v[3]
+    ]);
+  }
+
   target() {
     this.viewNormal = this.vnormalize(this.viewNormal);
     return(this.vplus(this.viewPoint, this.vscale(this.viewNormal, this.viewDistance)));
@@ -127,6 +185,19 @@ class View {
 
     this.viewNear = this.viewDistance - 0.5 * thickness;
     this.viewFar = this.viewDistance + 0.5 * thickness;
+  }
+
+  orbit (rightward, upward) {
+    let vTargetToOrigin = this.vscale(this.target(), -1);
+    let mTargetToOrigin = this.mtranslate(vTargetToOrigin);
+    let mAboutUp = this.mrotate(this.viewUp, rightward);
+    let mAboutRight = this.mrotate(this.viewRight, upward);
+    let mTargetFromOrigin = this.mtranslate(this.target());
+    let rotation = this.mmultiply(mTargetFromOrigin,
+                    this.mmultiply(mAboutRight,
+                      this.mmultiply(mAboutUp, mTargetToOrigin)));
+    let newViewPoint = this.mvmultiply(rotation, [...this.viewPoint,1]).slice(0,3);
+    this.look({from: newViewPoint});
   }
 }
 

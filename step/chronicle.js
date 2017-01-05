17f82707795a14bad4700b887c3f053367375ea4
Steve Pieper <pieper@isomics.com>
2017-01-05 09:11:22 -0500

    Clean up chronicle code and add tag/value query

--- step/chronicle.js
+++ step/chronicle.js
@@ -5,7 +5,7 @@ class Chronicle {
   }
 
   seriesOperation(options) {
-    let chronicle = options.database.chronicle || this.chronicle;
+    let chronicle = this.chronicle;
     let endKey = options.key.slice(0);
     endKey.push({});
     chronicle.query("instances/context", {
@@ -13,6 +13,39 @@ class Chronicle {
       end_key : endKey,
       reduce : true,
       group_level : 4,
+      stale : 'update_after',
+    }).then(function(data) {
+      let instanceURLs = [];
+      for (let rowIndex = 0; rowIndex < data.rows.length; rowIndex += 1) {
+        let row = data.rows[rowIndex].key;
+        let instanceUID = row[3];
+        let instanceURL = chronicle._db_name + "/" + instanceUID + '/object.dcm';
+        instanceURLs.push(instanceURL);
+      };
+      options.operation(instanceURLs);
+    }).catch(function (err) {
+      console.error(err);
+    });
+  }
+
+  /* example:
+    step.chronicle.tagValueOperation({
+      tag: "Modality",
+      value: "SR",
+      operation: console.log,
+      })
+  */
+  tagValueOperation(options) {
+    let chronicle = this.chronicle;
+    let tagEntry = DicomMetaDictionary.nameMap[options.tag];
+    let keyTag = DicomMetaDictionary.unpunctuateTag(tagEntry.tag);
+    let startKey = [keyTag, options.value];
+    let endKey = startKey.slice(0);
+    endKey.push({});
+    chronicle.query("tags/byTagAndValue", {
+      start_key : startKey,
+      end_key : endKey,
+      reduce : false,
       stale : 'update_after',
     }).then(function(data) {
       let instanceURLs = [];

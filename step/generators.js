pieper/sites

commit 160cf9cf2c374c4b6c49403a244fd23a13577b08

Refactor color handling and improve gradient opacity rendering

step/generators.js
@@ -160,7 +160,12 @@ class ProgrammaticGenerator extends Generator {
     let gl = this.gl;
     let location = gl.getUniformLocation(this.program, key);
     if (!location) {
-      console.error('No uniform location for', key);
+      // for now, don't warn for this since the field superclass
+      // declares visualization uniforms which may not be referenced
+      // in generator code.  This could be a source of confusion
+      // if uniforms aren't found in the shader code.
+      // TODO: add a verbose mode for diagnostics and debugging
+      //console.error('No uniform location for', key);
       return;
     }
 

step/fields/fiducial.js
@@ -9,10 +9,7 @@ class Fiducial {
 class FiducialField extends Field {
   constructor(options={}) {
     super(options);
-    this.rgba = options.rgba || [1., 0., 0., 0.5];
-    this.rgba = this.rgba.map(e=>e*1.00001); // hack to make it floating point
-    this.opacityScale = options.opacityScale || 1.;
-    this.opacityScale *= 1.00001; // hack to make it floating point
+    this.gradientOpacityScale = options.gradientOpacityScale || 1.;
     this.fiducials = options.fiducials || [];
     this.analyze();
   }
@@ -60,13 +57,16 @@ class FiducialField extends Field {
 
     let source = `
 
+      uniform vec4 rgba${this.id};
+      uniform float gradientOpacityScale${this.id};
       void transferFunction${this.id} (const in float sampleValue,
                                        const in float gradientMagnitude,
                                        out vec3 color,
                                        out float opacity)
       {
-          color = vec3( ${this.rgba[0]}, ${this.rgba[1]}, ${this.rgba[2]} );
-          opacity = ${this.opacityScale} * gradientMagnitude * sampleValue * ${this.rgba[3]};
+          color = vec3(rgba${this.id});
+          opacity = gradientOpacityScale${this.id} * gradientMagnitude *
+                    sampleValue * rgba${this.id}.a;
       }
 
       uniform ${this.samplerType} textureUnit${this.id};

step/fields/field.js
@@ -8,9 +8,17 @@ class Field {
     this.bounds = undefined; // the spatial extent of the field.
                              // undefined means there is no bound, otherwise
                              // an object with min and max
+
+    // viewing parameters
     this.visible = 1; // integer so it can be passed as a uniform
-    this.transformGain = 1.; // for visualizing/animating/exaggerating transform
-    this.generator = undefined; // the generator that populates the texture
+    this.rgba = options.rgba || [1., 1., 1., 1.]; // base color
+    this.rgba = this.rgba.map(e=>e*1.00001); // hack to make it floating point
+    this.gradientOpacityScale = options.gradientOpacityScale || 0.0005;
+
+    this.generator = options.generator || undefined; // the generator that populates the texture
+
+    // transform parameters
+    this.transformGain = options.transformGain || 1.; // for visualizing/animating/exaggerating transform
     this.transformField = undefined; // the field that defines the deformation
 
     if (this.useIntegerTextures) {
@@ -88,6 +96,8 @@ class Field {
     // return an object of the current uniform values
     let u = {};
     u['visible'+this.id] = {type: '1i', value: this.visible};
+    u['rgba'+this.id] = {type: '4fv', value: this.rgba};
+    u['gradientOpacityScale'+this.id] = {type: '1f', value: this.gradientOpacityScale};
     u['textureUnit'+this.id] = {type: '1i', value: this.id};
     if (this.transformField) {
       u['transformGain'+this.id] = {type: '1f', value: this.transformGain};

step/fields/image.js
@@ -23,9 +23,8 @@ class ImageField extends PixelField {
     // array of control points in form [value, {r, g, b, a}]
     this.transferFunction = [
       [0, {r: 0, g: 0, b: 0, a: 1}],
-      [1, {r: 255, g: 255, b: 255, a: 1}],
+      [1, {r: 1, g: 1, b: 1, a: 1}],
     ];
-    this.gradientOpacityScale = 1.;
   }
 
   statistics(options={}) {
@@ -77,11 +76,10 @@ class ImageField extends PixelField {
     u['windowWidth'+this.id] = {type: "1f", value: this.windowWidth};
     u['rescaleSlope'+this.id] = {type: "1f", value: this.rescaleSlope};
     u['rescaleIntercept'+this.id] = {type: "1f", value: this.rescaleIntercept};
-    u['gradientOpacityScale'+this.id] = {type: "1f", value: this.gradientOpacityScale};
     // add transfer function control point uniforms
     for (let index = 0; index < this.transferFunction.length; index++) {
       let [value, rgba] = this.transferFunction[index];
-      rgba = [rgba.r/255, rgba.g/255, rgba.b/255, rgba.a];
+      rgba = [rgba.r, rgba.g, rgba.b, rgba.a];
       u['tfcp'+this.id+'value'+index] = {type: '1f', value: value};
       u['tfcp'+this.id+'rgba'+index] = {type: '4fv', value: rgba};
     }
@@ -139,6 +137,7 @@ class ImageField extends PixelField {
     return(`
       uniform float windowCenter${this.id};
       uniform float windowWidth${this.id};
+      uniform vec4 rgba${this.id};
       uniform float gradientOpacityScale${this.id};
       ${this.transferFunctionControlPointUniformSource()}
       void transferFunction${this.id} (const in float sampleValue,
@@ -151,9 +150,14 @@ class ImageField extends PixelField {
                   / (windowWidth${this.id}-1.);
         pixelValue = clamp( pixelValue, 0., 1. );
 
-        ${this.transferFunctionControlPointLookupSource()}
-
-        opacity *= gradientMagnitude * gradientOpacityScale${this.id};
+        if (sliceMode == 1) {
+          color = vec3(pixelValue);
+          opacity = rgba${this.id}.a;
+        } else {
+          ${this.transferFunctionControlPointLookupSource()}
+          opacity *= gradientMagnitude * gradientOpacityScale${this.id};
+        }
+        color *= rgba${this.id}.rgb;
       }
     `);
   }

step/fields/segmentation.js
@@ -50,18 +50,20 @@ class SegmentationField extends PixelField {
       console.warn('SpacingBetweenSlices and SliceThickness should be equal for SEG');
       console.warn(pixelMeasures.SpacingBetweenSlices + ' != ' + pixelMeasures.SliceThickness);
     }
-    this.rgb = Colors.dicomlab2RGB(this.dataset.Segment[0].RecommendedDisplayCIELabValue);
+    this.rgba = Colors.dicomlab2RGB(this.dataset.Segment[0].RecommendedDisplayCIELabValue);
+    this.rgba.push(1);
+    this.gradientOpacityScale = 1;
   }
 
   uniforms() {
     let u = super.uniforms();
-    u['rgb'+this.id] = {type: '3fv', value: this.rgb};
     return(u);
   }
 
   transferFunctionSource() {
     return(`
-      uniform vec3 rgb${this.id};
+      uniform vec4 rgba${this.id};
+      uniform float gradientOpacityScale${this.id};
       void transferFunction${this.id} (const in float sampleValue,
                                        const in float gradientMagnitude,
                                        out vec3 color,
@@ -70,8 +72,8 @@ class SegmentationField extends PixelField {
         color = vec3(0., 0., 0.);
         opacity = 0.;
         if (sampleValue > 0.) {
-          color = sampleValue * rgb${this.id};
-          opacity = .1;
+          color = sampleValue * rgba${this.id}.rgb;
+          opacity = rgba${this.id}.a * gradientMagnitude * gradientOpacityScale${this.id};
         }
       }
     `);

step/ui.step.js
@@ -317,13 +317,13 @@ class stepDisplayMenu extends MenuPanel {
 
     // gradientOpacityScale
     this.gradientOpacityScale = new UI.Number();
-    this.gradientOpacityScale.value = 1;
     this.gradientOpacityScale.min = 0;
     this.gradientOpacityScale.precision = 6;
-    this.gradientOpacityScale.step = .25;
+    this.gradientOpacityScale.step = .00001;
     if (options.onGradientOpacityScaleChange) {
       this.gradientOpacityScale.onChange(options.onGradientOpacityScaleChange);
     }
+    this.gradientOpacityScale.setValue(0.0001);
     this.menuPanel.add( this.gradientOpacityScale );
 
     // scalar transfer function editor

commit 47f09268b9885d2ac13bbccb351871ebaaaeba46

Fix GPU filtering operations
Add sliceMode variable to FilterGenerator so that
images defined with lookup tables will know to
return raw pixel samples for filtering.

Fixes regression introduced by:
160cf9cf2c374c4b6c49403a244fd23a13577b08

step/generators.js
@@ -350,4 +350,11 @@ class FilterGenerator extends ProgrammaticGenerator {
     let pixelToTexture = pixelDimensions.map(e=>1./e);
     u['pixelToTexture'] = {type: '3fv', value: pixelToTexture};
   };
+
+  headerSource() {
+    return (`${super.headerSource()}
+      const int sliceMode = 1; // used for texture sampling (get value not transfer function)
+    `);
+  }
+
 }